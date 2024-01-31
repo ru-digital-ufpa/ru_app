3ef369f46aa26f0b06d604db9d046ad680e43dcb
prinako <nyankonton122@gmail.com>
2024-01-30 23:18:56 -0300

    a

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/prinako/.flutter_install/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/prinako/External/MyDrive/RU/ru_app"
+export "FLUTTER_APPLICATION_PATH=/home/prinako/Entenal/MyDrive/RU/ru_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
